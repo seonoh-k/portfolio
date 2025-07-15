3083788e63b3b54401b3efe036175443a7f2eb45
seonoh.k <kimseonoh101@gmail.com>
2025-07-15 18:10:38 +0900

    some changes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { motion } from 'framer-motion';
-import { FaJava, FaHtml5, FaCss3Alt, FaReact, FaAws, FaCloudflare } from "react-icons/fa";
+import { FaJava, FaHtml5, FaCss3Alt, FaReact, FaAws, FaCloudflare, FaGithub } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
-import { SiSpringboot } from "react-icons/si";
+import { SiSpringboot, SiNotion } from "react-icons/si";
 import { useTranslation } from "react-i18next";
 
 const front = [
@@ -31,40 +31,57 @@ export default function Home() {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -40 }}
             transition={{ duration: 0.8 }}
-            className="md:w-[970px]"
+            className="w-[320px] md:w-[970px]"
         >
-            <div className="grid grid-cols-1 md:grid-cols-2 p-8 md:p-[99px] text-white bg-gray-500 rounded-xl shadow text-center items-center shadow-lg">
+            <div className="grid grid-cols-1 md:grid-cols-2 p-6 md:p-16 text-white bg-gray-500 rounded-xl shadow text-center items-center shadow-lg">
                 <h1 className="text-2xl md:text-5xl opacity-70 md:text-left mb-10 md:mb-0">About</h1>
-                <div className="flex flex-col gap-2 opacity-70 md:gap-8 md:grid-cols-2 md:border-l border-gray-200 md:pl-6">
+                <div className="flex flex-col gap-2 opacity-70 md:gap-8 md:grid-cols-2 md:border-l border-gray-200 md:pl-6  space-y-6">
                     <div className="flex flex-col">
                         {t('about.lines', {returnObjects: true}).map((txt) => (
-                            <span key={txt} className="text-md md:text-2xl">{txt}</span>
+                            <span key={txt} className="text-md md:text-xl">{txt}</span>
                         ))}
                     </div>
-                    <div className="flex flex-col mt-10 text-lg">
+                    <div className="flex flex-col items-center text-lg">
                         <div className="flex items-center gap-1">
-                            <span className="font-semibold md:text-2xl">Frontend</span>
+                            <span className="font-semibold md:text-xl">Frontend</span>
                             {front.map((s, i) => (
-                                <div key={i} className="flex items-center gap-1 md:text-2xl">
+                                <div key={i} className="flex items-center gap-1 md:text-xl">
                                     {s.icon} {s.text}
                                 </div>
                             ))}
                         </div>
                         <div className="flex items-center gap-1">
-                            <span className="font-semibold md:text-2xl">Backtend</span>
+                            <span className="font-semibold md:text-xl">Backtend</span>
                             {back.map((b, i) => (
-                                <div key={i} className="flex items-center gap-1 md:text-2xl">
+                                <div key={i} className="flex items-center gap-1 md:text-xl">
                                     {b.icon} {b.text}
                                 </div>
                             ))}
                         </div>
                         <div className="flex items-center gap-1">
-                            <span className="font-semibold md:text-2xl">Infra</span>
+                            <span className="font-semibold md:text-xl">Infra</span>
                             {infra.map((i, idx) => (
-                                <div key={idx} className="flex items-center gap-1 md:text-2xl">
+                                <div key={idx} className="flex items-center gap-1 md:text-xl">
                                     {i.icon} {i.text}
                                 </div>
                             ))}
+                        </div>
+                    </div>
+                    <div className="flex flex-col text-white text-md md:text-xl justify-center items-center">
+                        {t('about.lines_2', {returnObjects: true}).map((txt) => (
+                            <span key={txt}>{txt}</span>
+                        ))}
+                        <div className="flex mt-6 justify-center items-center gap-4">
+                            <a href='https://www.notion.so/Study-22ff7b0856be80dcae43ffa200c858b9?source=copy_link' 
+                            target="_blank"
+                            className="flex border-b border-white hover:opacity-60 gap-2">
+                            <SiNotion className="mt-1"/> Notion
+                            </a>
+                            <a href='https://github.com/seonoh-k' 
+                                target="_blank" 
+                                className="flex border-b border-white hover:opacity-60 gap-2">
+                                <FaGithub className="mt-1"/> Github
+                            </a>
                         </div>
                     </div>
                 </div>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from 'framer-motion';
-import { IoMdMail } from "react-icons/io";
-import { FaGithub, FaPhoneAlt } from "react-icons/fa";
+import { FaGithub, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 import { SiNotion } from "react-icons/si";
 
 export default function Contact() {
@@ -14,23 +13,27 @@ export default function Contact() {
             transition={{ duration: 0.8 }}
             className="md:w-[970px]"
         >
-            <div className="grid grid-cols-1 md:grid-cols-2 px-16 py-8 md:p-20 bg-gray-500 rounded-xl shadow justify-center items-center shadow-lg">
+            <div className="grid grid-cols-1 md:grid-cols-2 px-16 py-8 md:p-16 bg-gray-500 rounded-xl shadow justify-center items-center shadow-lg">
                 <div className="flex flex-col items-center md:items-start mb-10 md:mb-0">
                     <h1 className="text-white text-2xl md:text-5xl opacity-70">Contact</h1>
                 </div>
                 <div className="flex flex-col gap-2 md:gap-8 md:border-l border-gray-200 md:pl-6">
-                    <a href='tel:01040289931' className="flex text-white text-md md:text-3xl opacity-60 gap-2">
+                    <a href='tel:01040289931' 
+                        className="flex text-white text-md md:text-3xl opacity-60 hover:opacity-40 gap-2">
                         <FaPhoneAlt className="mt-1"/> +82-10-4028-9931
                     </a>
-                    <a href='mailto:kimseonoh101@gmail.com' className="flex text-white text-md md:text-3xl opacity-60 gap-2">
-                        <IoMdMail className="mt-1"/> kimseonoh101@gmail.com
+                    <a href='mailto:kimseonoh101@gmail.com' 
+                        className="flex text-white text-md md:text-3xl opacity-60 hover:opacity-40 gap-2">
+                        <FaEnvelope className="mt-1"/> kimseonoh101@gmail.com
                     </a>
-                    <a href='https://github.com/seonoh-k' target="_blank" className="flex text-white text-md md:text-3xl opacity-60 gap-2">
+                    <a href='https://github.com/seonoh-k' 
+                        target="_blank" 
+                        className="flex text-white text-md md:text-3xl opacity-60 hover:opacity-40 gap-2">
                         <FaGithub className="mt-1"/> Github
                     </a>
                     <a href='https://www.notion.so/Study-22ff7b0856be80dcae43ffa200c858b9?source=copy_link' 
                         target="_blank"
-                        className="flex text-white text-md md:text-3xl opacity-60 gap-2">
+                        className="flex text-white text-md md:text-3xl opacity-60 hover:opacity-40 gap-2">
                         <SiNotion className="mt-1"/> Notion
                     </a>
                 </div>
